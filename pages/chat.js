49810c25d760223a300a5ca3202f470e05c465b7
KansasMyers <kansasmyers360@gmail.com>
2022-01-28 20:37:20 -0300

    feat: Aula 03 concluida com Exercicio do botão.

--- pages/chat.js
+++ pages/chat.js
@@ -1,7 +1,203 @@
+import { Box, Text, TextField, Image, Button } from "@skynexui/components";
+import { useState } from "react";
+import appConfig from "../config.json";
+
 export default function ChatPage() {
+  const [message, setMessage] = useState("");
+  const [listMessage, setListMessage] = useState([]);
+
+  function handleSubmitMessage(newMessage) {
+    const message = {
+      text: newMessage,
+      from: "KansasMyers",
+      id: listMessage.length + 1
+    };
+
+    setListMessage([message, ...listMessage]);
+    setMessage("");
+  }
+
+  return (
+    <Box
+      styleSheet={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: appConfig.theme.colors.primary[500],
+        backgroundImage: `url(https://i.pinimg.com/originals/a0/73/2f/a0732f8b3693f76363d6e5307ec43996.jpg)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundBlendMode: "multiply",
+        color: appConfig.theme.colors.neutrals["000"],
+      }}
+    >
+      <Box
+        styleSheet={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+          borderRadius: "5px",
+          backgroundColor: appConfig.theme.colors.neutrals[700],
+          height: "100%",
+          maxWidth: "95%",
+          maxHeight: "95vh",
+          padding: "32px",
+        }}
+      >
+        <Header />
+        <Box
+          styleSheet={{
+            position: "relative",
+            display: "flex",
+            flex: 1,
+            height: "80%",
+            backgroundColor: appConfig.theme.colors.neutrals[600],
+            flexDirection: "column",
+            borderRadius: "5px",
+            padding: "16px",
+          }}
+        >
+          <MessageList messages={listMessage} />
+
+          <Box
+            as="form"
+            onSubmit={(event) => {
+              event.preventDefault();
+              handleSubmitMessage(message);
+            }}
+            styleSheet={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <TextField
+              placeholder="Insira sua mensagem aqui..."
+              value={message}
+              onChange={(event) => {
+                setMessage(event.target.value);
+              }}
+              onKeyPress={(event) => {
+                if (event.key === "Enter") {
+                  event.preventDefault();
+                  handleSubmitMessage(message);
+                }
+              }}
+              type="textarea"
+              styleSheet={{
+                width: "100%",
+                border: "0",
+                resize: "none",
+                borderRadius: "5px",
+                padding: "6px 8px",
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                marginRight: "12px",
+                color: appConfig.theme.colors.neutrals[200],
+              }}
+            />
+            <Button
+              type="submit"
+              label="Enviar"
+              styleSheet={{
+                marginBottom: '10px'
+              }}
+              buttonColors={{
+                contrastColor: appConfig.theme.colors.neutrals["000"],
+                mainColor: appConfig.theme.colors.primary[500],
+                mainColorLight: appConfig.theme.colors.primary[400],
+                mainColorStrong: appConfig.theme.colors.primary[600],
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+function Header() {
   return (
     <>
-      <h1>Chat Page!</h1>
+      <Box
+        styleSheet={{
+          width: "100%",
+          marginBottom: "16px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text variant="heading5">Chat</Text>
+        <Button
+          variant="tertiary"
+          colorVariant="neutral"
+          label="Logout"
+          href="/"
+        />
+      </Box>
     </>
+  );
+}
+
+function MessageList(props) {
+  console.log("MessageList", props);
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflow: "scroll",
+        display: "flex",
+        flexDirection: "column-reverse",
+        flex: 1,
+        color: appConfig.theme.colors.neutrals["000"],
+        marginBottom: "16px",
+      }}
+    >
+      {props.messages.map((message) => {
+        return (
+          <Text
+            key={message.id}
+            tag="li"
+            styleSheet={{
+              borderRadius: "5px",
+              padding: "6px",
+              marginBottom: "12px",
+              hover: {
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+              },
+            }}
+          >
+            <Box
+              styleSheet={{
+                marginBottom: "8px",
+              }}
+            >
+              <Image
+                styleSheet={{
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                  display: "inline-block",
+                  marginRight: "8px",
+                }}
+                src={`https://github.com/${message.from}.png`}
+              />
+              <Text tag="strong">{message.from}</Text>
+              <Text
+                styleSheet={{
+                  fontSize: "10px",
+                  marginLeft: "8px",
+                  color: appConfig.theme.colors.neutrals[300],
+                }}
+                tag="span"
+              >
+                {new Date().toLocaleDateString()}
+              </Text>
+            </Box>
+            {message.text}
+          </Text>
+        );
+      })}
+    </Box>
   );
 }
